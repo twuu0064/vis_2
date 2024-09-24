5e7ccc391734513a09dcfe2aea5d9dac85879225
Tanisha <twuu0064@student.monash.edu>
2024-09-24 17:10:53 +1000

    working to embed vis

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_2 = "./population_density.vg.json";
+vegaEmbed("#pop_density", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+        }).catch(console.error);
