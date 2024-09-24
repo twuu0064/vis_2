4fcd8a54a1908289482475420707ca565eb00a67
Tanisha <twuu0064@student.monash.edu>
2024-09-24 17:30:45 +1000

    embed chart to html

--- js/vega_lite_vis.js
+++
@@ -1,4 +0,0 @@
-var vg_2 = "./population_density.vg.json";
-vegaEmbed("#pop_density", vg_2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
-        }).catch(console.error);
